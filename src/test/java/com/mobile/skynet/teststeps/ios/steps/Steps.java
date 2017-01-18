5689c141ed9c9d40a5abae7247c7cdbf1b4a1fbd
pjmadan <prajit.madan@gmail.com>
2017-01-18 14:42:46 -0500

    Added IOS driver initialization

--- src/test/java/com/mobile/skynet/teststeps/ios/steps/Steps.java
+++ src/test/java/com/mobile/skynet/teststeps/ios/steps/Steps.java
@@ -1,16 +1,29 @@
 package com.mobile.skynet.teststeps.ios.steps;
 
+import com.mobile.skynet.picocontainers.AppiumDriverContainer;
+import com.mobile.skynet.screenobjects.HomeScreen;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import org.junit.Assert;
 
 /**
  * Created by prajitmadan on 1/10/17.
  */
 public class Steps {
+    private AppiumDriver<? extends MobileElement> driver;
+    private AppiumDriverContainer appiumDriverContainer;
+
+    public Steps(AppiumDriverContainer appiumContainer) {
+        this.appiumDriverContainer = appiumContainer;
+    }
     @Given("^an end user has installed the Deportes iOS application and has launched it and is on the home screen$")
     public void an_end_user_has_installed_the_Deportes_iOS_application_and_has_launched_it_and_is_on_the_home_screen() throws Throwable {
-
+        driver = appiumDriverContainer.getDriver("ios");
+        HomeScreen homeScreen = new HomeScreen(driver);
+        Assert.assertTrue(homeScreen.istextpresent("HelloCharts"));
     }
 
     @When("^I click on the first content displayed on the app$")
